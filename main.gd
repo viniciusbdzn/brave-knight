2491960c0bf5309ec3791d279e33d86a84a3305a
Vinicius Badazoni <95323715+viniciusbdzn@users.noreply.github.com>
2024-07-23 20:14:04 -0300

    enviando arquivos do projeto

---
+++ main.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+@export var game_ui: CanvasLayer
+@export var game_over_ui_template: PackedScene
+
+#Assim que o signal for ouvido, ele conecta e chama a função "trigger_game_over()"
+func _ready():
+	GameManager.game_over.connect(trigger_game_over)
+
+func trigger_game_over():
+	#Deletar GameUI quando o jogo acabar
+	if game_ui:
+		game_ui.queue_free()
+		game_ui = null
+	
+	#Criar GameOverUI
+	var game_over_ui: GameOverUI = game_over_ui_template.instantiate()
+	add_child(game_over_ui)
